cd53ee96f417ed2b0eb510ee77c80938f4eccc62
Nate <n.s.trump@gmail.com>
2020-08-16 17:13:27 -0400

    Finished basic flex cards.

--- js/active.js
+++ js/active.js
@@ -4,9 +4,25 @@
 $(document).ready(function() {
     
     $(".card").click(function() {
-        $(".card").removeClass(" active");
-        $(".card__banner").removeClass("fadeIn--fast");
-        $(this).addClass(" active");
-        $(this).children(".card__banner").addClass("fadeIn--fast");
+        if ($(this).hasClass("active")) {
+            
+        } else {
+            $(".card").removeClass(" active");
+            $(".card__banner").removeClass("fadeIn--fast");
+            $(this).addClass(" active");
+            $(this).children(".card__banner").addClass("fadeIn--fast");
+        }
      });
+
+    $(".card__btn").click(function() {
+        if($(this).parent().hasClass("card__info-spanner")) {
+            $(this).siblings(".card__info").css("display", "none");
+            $(this).parent().removeClass(" card__info-spanner");
+            $(this).text("Learn More");  
+        } else {
+            $(this).parent().addClass(" card__info-spanner");
+            $(this).siblings(".card__info").css("display", "block");
+            $(this).text("Close");
+        }
+    });
 });
